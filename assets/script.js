d86e0578100d868d66df196394924db3a1d5ca62
Wilson Sous <souswilson@gmail.comgit config --list>
2021-09-08 13:38:01 -0400

    updated script.js

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 //  Function to gather current time/date
 function createSchedule() {
 
-    let todayDate = getDateNow();
-    let hourNow = getHourNow();
+    let todayDate = getCurrentDate();
+    let hourNow = getCurrentHour();
     $("#currentDay").append(todayDate);
   
-    // Selected time frames
+    // Selected time frame array
     let timeFrame = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
   
     for( let j = 0; j < 9; j++){
@@ -13,8 +13,10 @@ function createSchedule() {
         localStorage.setItem(timeFrame[j], "");
       }
     }
+
+
     
-    // Functionality of save planner button
+    // Functionality of save planner time/description/save button in order
     for (let i = 9; i < 18; i++) {
       let timeArea = $("<textArea disabled class='time-block hour col-md-1'>" + timeFrame[i-9] + "</textArea>");
       let descriptionBox = $("<textArea class='description "+ "description" + i +" col-md-10' id='"+ timeFrame[i-9]+"'>"+localStorage.getItem(timeFrame[i-9])+"</textArea>");
@@ -25,7 +27,7 @@ function createSchedule() {
       $(".description"+i).addClass(getPastPresentFuture(i,hourNow));
     }
   }
-  
+
   // Functions for text box descriptions
   function callSaveDescriptionBox(key){
     console.log("callSaveDescriptionBox " + key);
@@ -37,14 +39,15 @@ function createSchedule() {
     localStorage.setItem(key, value);
   }
   
+  
   // Reset/Save Functionality
   createSchedule();
   
-  function getDateNow(){
+  function getCurrentDate(){
     return moment().format('dddd, MMMM Do YYYY');
   }
   
-  function getHourNow(){
+  function getCurrentHour(){
     return moment().hour();
   }
   
